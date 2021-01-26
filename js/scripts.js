9349986435c8b2d4974a2d504290e193791e608c
Jeremy Miller <gkstaff@gmail.com>
2021-01-26 15:35:08 -0800

    adds in the last parts of the Address book. Jer Miller <gkstaff@gmail.com> Matthew Foley <mattfoley0375@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,24 @@ function AddressBook() {
   this.currentId = 0;
 }
 //Properties to invoke when passed to addressBook. 
-AddressBook.prototype.addContact = function(contact) {
+AddressBook.prototype.addContact = function (contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
 }
 
-AddressBook.prototype.assignId = function() {
+AddressBook.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
 }
 
-AddressBook.prototype.findContact = function(id) {
+AddressBook.prototype.findContact = function (id) {
   if (this.contacts[id] != undefined) {
     return this.contacts[id];
   }
   return false;
 }
 
-AddressBook.prototype.deleteContact = function(id) {
+AddressBook.prototype.deleteContact = function (id) {
   if (this.contacts[id] === undefined) {
     return false;
   }
@@ -37,33 +37,42 @@ function Contact(firstName, lastName, phoneNumber) {
   this.phoneNumber = phoneNumber;
 }
 
-Contact.prototype.fullName = function() {
+Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 }
 
 // User Interface Logic ---------
 let addressBook = new AddressBook();
 
-function displayContactDetails(addressBookToDisplay){
+function displayContactDetails(addressBookToDisplay) {
   let contactsList = $("ult#contacts");
-  let htmlforContactInfo="";
-  Object.keys(addressBookToDisplay.contacts).forEach(function(key){
-    const contact = adressBookToDisplay.findContact(key);
-    htmlforContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>"
-  
-  contactsList.html(htmlforContactInfo);
-  }
-)};
+  let htmlforContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function (key) {
+    const contact = addressBookToDisplay.findContact(key);
+    htmlforContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+    contactsList.html(htmlforContactInfo);
+  })
+};
 
-$(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function () {
+    console.log("The id of this <li> is " + this.id + ".");
+  });
+}
+
+
+
+$(document).ready(function () {
+  attachContactListeners();
+  $("form#new-contact").submit(function (event) {
     event.preventDefault();
     const inputtedFirstName = $("input#new-first-name").val();
     const inputtedLastName = $("input#new-last-name").val();
     const inputtedPhoneNumber = $("input#new-phone-number").val();
     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
-    console.log(addressBook.contacts);
+    displayContactDetails(addressBook);
   });
 
 });
